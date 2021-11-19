7411e01febad3d1b8415a46a6f52a7b28596f248
saverio87 <mattolisaverio@gmail.com>
2021-11-19 16:53:51 +0800

    Build created locally

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
